c551ba34a3ca56ffc60863158e4f479cc7073da2
Tiago Vieira <vieira.tiagofraga@gmail.com>
2019-06-15 15:33:29 -0300

    alterações do gulp, bower, bootstrap e etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('sass', function () {
     return gulp.src('sass/**/*.sass')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('css'));
 });
 
